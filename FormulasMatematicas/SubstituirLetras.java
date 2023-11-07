162d880290be9af7813e3658082eabd7f2dd2ae0
liviaesmeraldo <you@example.comliviaesmeraldo@gmail.com>
2023-11-07 12:53:01 -0300

    Adiciona a questão 18

---
+++ FormulasMatematicas/SubstituirLetras.java
@@ -0,0 +1,18 @@
+package FormulasMatematicas;
+
+import java.util.Scanner;
+
+public class SubstituirLetras {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite uma frase:");
+        String frase = scanner.nextLine();
+
+        String novaFrase = frase.replace("a", "e");
+
+        System.out.println("Frase modificada: " + novaFrase);
+
+        scanner.close();
+    }
+}
