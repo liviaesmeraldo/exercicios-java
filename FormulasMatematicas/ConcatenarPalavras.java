9dd61dec0925801dc2594052755ee67e914653ad
liviaesmeraldo <you@example.comliviaesmeraldo@gmail.com>
2023-11-07 11:57:57 -0300

    Adiciona a questão 16

---
+++ FormulasMatematicas/ConcatenarPalavras.java
@@ -0,0 +1,21 @@
+package FormulasMatematicas;
+
+import java.util.Scanner;
+
+public class ConcatenarPalavras {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a primeira palavra: ");
+        String palavra1 = scanner.nextLine();
+
+        System.out.print("Digite a segunda palavra: ");
+        String palavra2 = scanner.nextLine();
+
+        String resultado = palavra1 + palavra2;
+
+        System.out.println("A palavra resultante da concatenação é: " + resultado);
+
+        scanner.close();
+    }
+}
