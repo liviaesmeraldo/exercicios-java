68ddee152496e852638d7042c6ce377ecc35e03a
liviaesmeraldo <you@example.comliviaesmeraldo@gmail.com>
2023-11-07 14:16:13 -0300

    Adiciona questão 29

--- Condicionais/ParOuImpar.java
+++ Condicionais/ParOuImpar.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class ParOuImpar {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in)
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite um número");
+        System.out.print("Digite um número: ");
         int numero = scanner.nextInt();
 
         if (numero % 2 == 0){
