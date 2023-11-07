75829bd26986a4b51c09426e25665e328a1d57ca
liviaesmeraldo <you@example.comliviaesmeraldo@gmail.com>
2023-11-07 11:46:01 -0300

    Adiciona a questão 5

---
+++ FormulasMatematicas/CalculadoraImc.java
@@ -0,0 +1,22 @@
+package FormulasMatematicas;
+
+import java.util.Scanner;
+
+public class CalculadoraImc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o peso em kg: ");
+        double peso = scanner.nextDouble();
+
+        System.out.print("Digite a altura em metros:");
+        double altura = scanner.nextDouble();
+
+        double imc = peso / (altura*altura);
+
+        System.out.println("O IMC é: " + imc);
+
+        scanner.close();
+    }
+    
+}
