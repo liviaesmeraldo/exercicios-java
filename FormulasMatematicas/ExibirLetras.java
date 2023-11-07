bd642529149794637b53d7904f0ea013ca24da0b
liviaesmeraldo <you@example.comliviaesmeraldo@gmail.com>
2023-11-07 12:10:30 -0300

    Adicione a questão 17 versão 1

---
+++ FormulasMatematicas/ExibirLetras.java
@@ -0,0 +1,21 @@
+package FormulasMatematicas;
+
+import java.util.Scanner;
+
+public class ExibirLetras {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite uma palavra");
+        String palavra = scanner.nextLine();
+
+        for (int i = 0; i < palavra.length(); i ++){
+            char letra = palavra.charAt(i);
+            System.out.println(letra);
+
+        }
+        
+        scanner.close();
+
+    }
+}
