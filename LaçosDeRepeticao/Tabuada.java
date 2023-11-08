013a35b78cd639289d34329ac2611694f89d34b8
liviaesmeraldo <you@example.comliviaesmeraldo@gmail.com>
2023-11-08 09:28:05 -0300

    Adiciona questão 50

---
+++ LaçosDeRepeticao/Tabuada.java
@@ -0,0 +1,20 @@
+package LaçosDeRepeticao;
+
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Tabuada do " + numero + ":");
+        for (int i = 1; i <= 10; i++) {
+            int resultado = numero * i;
+            System.out.println(numero + "x" + i + "=" + resultado);
+        }
+        scanner.close();
+    }
+    
+}
