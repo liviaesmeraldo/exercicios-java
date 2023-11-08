2707626dad088940e664dcb67e1717687cfad4d2
liviaesmeraldo <you@example.comliviaesmeraldo@gmail.com>
2023-11-08 09:04:56 -0300

    Adiciona questão 48

---
+++ LaçosDeRepeticao/NumerosPares.java
@@ -0,0 +1,12 @@
+package LaçosDeRepeticao;
+
+public class NumerosPares {
+    public static void main(String[] args) {
+        int i = 2;
+        while (i <= 100) {
+            System.out.println(i);
+            i += 2;
+        }
+    }
+    
+}
